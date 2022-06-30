ab0a5f845d9747c17eb63c876e75847965728619
zikri21 <marifatululfah99@gmail.com>
2022-06-30 10:23:55 +0700

    CRUD

---
+++ CodinganCRUD/index.php
@@ -0,0 +1,59 @@
+<?php
+include("koneksi.php");
+
+// query untuk menampilkan data
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Data Barang</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Data Barang</h1>
+        <div class="main">
+            <a href="tambah.php">Tambah Barang</a>
+            <table>
+            <tr>
+                <th>Gambar</th>
+                <th>Nama Barang</th>
+                <th>Katagori</th>
+                <th>Harga Jual</th>
+                <th>Harga Beli</th>
+                <th>Stok</th>
+                <th>Aksi</th>
+            </tr>
+            <?php if($result): ?>
+            <?php while($row = mysqli_fetch_array($result)): ?>
+            <tr>
+                <td><img src="gambar/<?= $row['gambar'];?>" alt="<?=$row['nama'];?>"></td>
+                <td><?= $row['nama'];?></td>
+                <td><?= $row['kategori'];?></td>
+                <td><?= $row['harga_jual'];?></td>
+                <td><?= $row['harga_beli'];?></td>
+                <td><?= $row['stok'];?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $row['id_barang'];?>">Ubah</a>
+                    <a href="hapus.php?id=<?= $row['id_barang'];?>">Hapus</a> 
+                </td>
+            </tr>
+            <?php endwhile; else: ?>
+            <tr>
+                <td colspan="7">Belum ada data</td>
+            </tr>
+            <?php endif; ?>
+            </table>
+        </div>
+    </div>
+</body>
+<footer>
+    <h3>Nama    : ZIKRI HADIANSYAH</h3>
+    <h3>NIM     : 312010069</h3>
+    <h3>Kelas : TI.20.D.1</h3>
+</footer>
+</html>
